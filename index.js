70e86704bde53767b31b00e32d577021a92eecff
kat-star <katherinewkwong@gmail.com>
2019-11-07 21:48:52 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,69 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+  let newArr = [];
+  for (let i = 0; i < sourceArray.length; i++) {
+    newArr.push(sourceArray[i] * -1)
+  }
+  return newArr;
+}
+
+function mapToNoChange(sourceArray) {
+  let newArr = [];
+  for (let i = 0; i < sourceArray.length; i++) {
+    newArr.push(sourceArray[i])
+  }
+  return newArr;
+}
+
+function mapToDouble(sourceArray) {
+  let newArr = [];
+  for (let i = 0; i < sourceArray.length; i++) {
+    newArr.push(sourceArray[i] * 2)
+  }
+  return newArr;
+}
+
+function mapToSquare(sourceArray) {
+  let newArr = [];
+  for (let i = 0; i < sourceArray.length; i++) {
+    newArr.push(sourceArray[i] ** 2)
+  }
+  return newArr;
+}
+
+function reduceToTotal(sourceArray, startingPoint) {
+  let newCollection = sourceArray;
+  let result = startingPoint
+
+  if (!startingPoint) {
+    result = sourceArray[0];
+    newCollection = sourceArray.slice(1);
+  }
+
+  for (let i = 0; i < newCollection.length; i++) {
+    result = result + newCollection[i]
+  }
+  return result;
+}
+
+function reduceToAllTrue(sourceArray) {
+  let result = true;
+
+  for (let i = 0; i < sourceArray.length; i++) {
+    if (!sourceArray[i]) {
+      return false;
+    }
+  }
+  return result;
+}
+
+function reduceToAnyTrue(sourceArray) {
+  let result = false;
+
+  for (let i = 0; i < sourceArray.length; i++) {
+    if (sourceArray[i]) {
+      return true;
+    }
+  }
+  return result;
+}
